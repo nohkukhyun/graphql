b9989b1a71e88e7b2de9921a6c104bf2f910153b
kukhyunnoh <bbi9l@naver.com>
2020-10-26 14:16:38 +0900

    setting completed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App(){
+  return(
+    <div>
+      오 드디어 되는건가
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports={
+  mode: 'development',
+  entry: './src/index.tsx',
+  output:{
+    path: path.resolve(__dirname + './dist'),
+    filename:'[name].js'
+  },
+  devServer: {
+    contentBase: path.resolve("./dist"),
+    publicPath:"/",
+    port: 3000,
+    overlay: true,
+    stats: "errors-only",
+    historyApiFallback:true
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    // alias: {
+    //   'components': path.resolve(__dirname, '..', 'src', 'components'),
+    //   'containers': path.resolve(__dirname, '..', 'src', 'containers'),
+    //   'assets': path.resolve(__dirname, '..', 'src', 'assets'),
+    // },
+  },
+  module:{
+    rules:[
+      {
+        test:/\.ts(x?)$/,
+        exclude:/node_modules/,
+        use: [
+          {
+            loader:'ts-loader'
+          }
+        ]
+      },
+      {
+        test:/\.scss$/,
+        use:[
+          'style-loader','css-loader','sass-loader'
+        ]
+      },
+      {
+        test:/\.(jpe?g|png|gif|svg)$/,
+        use:[
+          {
+            loader:'file-loader',
+            options:{
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({template:'./public/index.html'})
+  ]
+}
